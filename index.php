0ba86db2ddc02f18b9cd4575f042bafe30f2a13a
Alioune <johnsonalioune@gmail.com>
2016-01-15 04:30:57 +0000

    Création du fichier index.php

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>	
+		<meta charset="utf-8" />
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<title>Tool Bi - Site d'annonces, d'achat et de vente pour les agriculteurs et opérateurs du Sénégal et de l'Afrique.</title>
+
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+      		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+	</head>
+
+	<body>
+		<div class="container-fluid">
+			<!-- Entête de page -->
+			<header>
+				<nav class="navbar">
+					<div class="navbar-header col-md-9">
+					<a class="navbar-brand" href="#">Tool Bi</a>
+					</div>
+					<ul class="nav navbar-nav col-md-3">
+						<li class="dropdown">
+							<a data-toggle="dropdown" href="#">Mon compte <b class="caret"></b></a>
+							<ul class="dropdown-menu dropdown-menu-right">
+								<li><a href="#">Inscription</a></li>
+								<li><a href="#">Connexion</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Publier une annonce</a></li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+
+		<!-- barre de recherche avec image de fond -->
+		<div class="carousel slide" data-ride="carousel">
+			<div class="carousel-inner">
+
+				<div class="item active" style="background-image:url('img/champs-cereale.jpg');">
+					<div class="container-fluid">
+						<h1 class="col-md-offset-2 col-md-8" style="padding-top: 50px; color:white;">Vendez gratuitement vos récoltes sur <strong>Tool Bi</strong>.sn</h1>
+						<h1 class="col-md-offset-3 col-md-6"><small style="color:white;">Platforme de commerce entre agriculteurs et opérateurs</small></h1>
+						<form class="form-inline">
+							<div style="padding-bottom: 189px;"class="input-group col-md-offset-3 col-md-6">
+								<input type="text" class="form-control input-lg" placeholder="Taper le produit que vous recherchez. Ex: riz" />
+								<span class="input-group-btn">
+									<button class="btn btn-success btn-lg" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div><!-- fin du premier carousel -->
+
+				<div class="item" style="background-image:url('img/champs-cereale-2.jpg');">
+					<div class="container-fluid">
+						<h1 class="col-md-offset-2 col-md-8" style="padding-top: 50px; color:white;">Vendez gratuitement vos récoltes sur <strong>Tool Bi</strong>.sn</h1>
+						<h1 class="col-md-offset-3 col-md-6"><small style="color:white;">Platforme de commerce entre agriculteurs et opérateurs</small></h1>
+						<form class="form-inline">
+							<div style="padding-bottom: 189px;"class="input-group col-md-offset-3 col-md-6">
+								<input type="text" class="form-control input-lg" placeholder="Taper le produit que vous recherchez. Ex: riz" />
+								<span class="input-group-btn">
+									<button class="btn btn-success btn-lg" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div><!-- fin du deuxième carousel -->
+
+				<div class="item" style="background-image:url('img/legumes.jpg');">
+					<div class="container-fluid">
+						<h1 class="col-md-offset-2 col-md-8" style="padding-top: 50px; color:white;">Vendez gratuitement vos récoltes sur <strong>Tool Bi</strong>.sn</h1>
+						<h1 class="col-md-offset-3 col-md-6"><small style="color:white;">Platforme de commerce entre agriculteurs et opérateurs</small></h1>
+						<form class="form-inline">
+							<div style="padding-bottom: 189px;"class="input-group col-md-offset-3 col-md-6">
+								<input type="text" class="form-control input-lg" placeholder="Taper le produit que vous recherchez. Ex: riz" />
+								<span class="input-group-btn">
+									<button class="btn btn-success btn-lg" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div><!-- fin du troisième carousel -->
+
+				<div class="item" style="background-image:url('img/arachides.jpg');">
+					<div class="container-fluid">
+						<h1 class="col-md-offset-2 col-md-8" style="padding-top: 50px; color:white;">Vendez gratuitement vos récoltes sur <strong>Tool Bi</strong>.sn</h1>
+						<h1 class="col-md-offset-3 col-md-6"><small style="color:white;">Platforme de commerce entre agriculteurs et opérateurs</small></h1>
+						<form class="form-inline">
+							<div style="padding-bottom: 189px;"class="input-group col-md-offset-3 col-md-6">
+								<input type="text" class="form-control input-lg" placeholder="Taper le produit que vous recherchez. Ex: riz" />
+								<span class="input-group-btn">
+									<button class="btn btn-success btn-lg" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div><!-- fin du dernier carousel -->
+
+			</div>
+		</div>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+   
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	</body>
+
+</html>
